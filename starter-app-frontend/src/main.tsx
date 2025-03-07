3a6650622701fce5d07b1814af0fbf3c0d8f9c0d
Chris Rogers <christopher.rogers@lmi.org>
2025-03-07 15:36:06 -0500

    cleanup some front end crap

--- starter-app-frontend/src/main.tsx
+++ starter-app-frontend/src/main.tsx
@@ -5,8 +5,10 @@ import App from './App.tsx'
 import { AuthProvider } from 'react-oidc-context'
 
 const oidcConfig = {
-  authority: "http://localhost:9000/application/o/starter-app",
-  client_id: "D8iHSd6v8we8O41q0GIEvQxYeaVDJHsa5r13cnHZ",
+  //authority: "http://localhost:9000/application/o/starter-app-2",
+  //client_id: "D8iHSd6v8we8O41q0GIEvQxYeaVDJHsa5r13cnHZ",
+  authority: "http://localhost:9000/application/o/starter-app-3-with-nate-moody/",
+  client_id: "tZXSSTAHmiUKKikSDQVq3vaSy6YwTKuIt2kyTDlR",
   redirect_uri: "http://localhost:5173",
   response_type: "code",
   scope: "openid profile email offline_access"
@@ -14,7 +16,7 @@ const oidcConfig = {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider {...oidcConfig}>
+    <AuthProvider {...oidcConfig}> 
       <App />
     </AuthProvider>
   </StrictMode>,
